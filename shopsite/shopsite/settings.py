16333650dea6bbfc7762bcf6c7f9a0d24e453406
vincent <52755752+god-of-thunder@users.noreply.github.com>
2023-05-18 17:28:17 +0800

    feat: django use mysql db

--- shopsite/shopsite/settings.py
+++ shopsite/shopsite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'shopsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cdp_organization',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'mysql',
+        'PORT': '3306',
     }
 }
 
